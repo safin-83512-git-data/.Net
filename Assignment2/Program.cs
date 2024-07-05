da6d877e2fecb0ab4ff9811952c21d5755ddb032
Safin Tamboli <89857881+kd2-safin-83512@users.noreply.github.com>
2024-07-05 22:48:26 +0530

    Assignment Uploaded 2

---
+++ Assignment2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CommandLineCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: CommandLineCalculator <num1> <operation> <num2>");
+                return;
+            }
+
+            double num1 = Convert.ToDouble(args[0]);
+            string operation = args[1];
+            double num2 = Convert.ToDouble(args[2]);
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
