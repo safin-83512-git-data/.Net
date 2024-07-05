f8f24a37b5617606a895d86f6be0cc4eddf117ad
Safin Tamboli <89857881+kd2-safin-83512@users.noreply.github.com>
2024-07-05 22:49:07 +0530

    Assignment Uploaded 3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueCalculating = true;
+
+            do
+            {
+                Console.WriteLine("Enter first number:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operation (+, -, *, /):");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Enter second number:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                double result = 0;
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation.");
+                        continue;
+                }
+
+                Console.WriteLine("Result: " + result);
+
+                Console.WriteLine("Do you want to perform another calculation? (y/n)");
+                continueCalculating = Console.ReadLine().ToLower() == "y";
+
+            } while (continueCalculating);
+        }
+    }
+}
